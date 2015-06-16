42e52db401c049c49372b8650c29b7a613337df4
hpetrow <heather.petrow@gmail.com>
2015-06-16 17:14:51 -0400

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,11 @@
 class Song
   # code goes here
+  attr_accessor :title, :artist
+
+  def serialize
+    filename = self.title.downcase.gsub(" ", "_");
+    file = Tempfile.new(["#{filename}", ".txt"], "tmp/")
+    file.write("#{self.artist.name} - #{self.title}")
+    file.rewind
+  end
 end
